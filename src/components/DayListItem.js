26b57b005938e6161645a549ee282ef07eb7c93a
SooyeonCheon123 <suyeon5445@gmail.com>
2023-03-22 18:09:23 -0700

    build datalist component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,26 @@
 import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+  let dayClass = classNames('day-list__item', {
+    'day-list__item--selected': props.selected,
+    'day-list__item--full': props.spots===0
+  });
+
+  const formatSpots = () => {
+    if (props.spots === 0) return "no spots remaining";
+    else if (props.spots === 1) return "1 spot remaining";
+    else {
+      return `${props.spots} spots remaining`;
+    }
+  }
+
+
   return (
-    <li onClick={() => props.SetDay(props.name)}>
+    <li className={dayClass} selected={props.selected} onClick={() => props.SetDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
